29eb83b3bade40487534733db1fa7c96484db688
pavli22 <bavlysamy651@gmail.com>
2024-11-04 23:47:26 +0200

    updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,44 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatButtonModule } from '@angular/material/button';
+import { NewHospitalComponent } from '../new-hospital/new-hospital.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MatButtonModule,
-    MatStepperModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
+    NewHospitalComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  isLinear = false;
-  hospitalForm!: FormGroup;
-  constructor(private fb: FormBuilder) { }
+
+  constructor() { }
   ngOnInit(): void {
-    this.hospitalForm = this.fb.group({
-      hospitalName: [''],
-      contactNo: [''],
-      ownerName: [''],
-      ownerContactNo: [''],
-      ownerEmailId: [''],
-      city: [''],
-      address: [''],
-      userName: [''],
-      password: ['']
-    });
-  }
-  onSubmit() {
 
   }
-  onReset() {
 
-  }
 }
--- src/app/pages/new-hospital/new-hospital.component.ts
+++ src/app/pages/new-hospital/new-hospital.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-new-hospital',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './new-hospital.component.html',
   styleUrl: './new-hospital.component.scss'
 })
-export class NewHospitalComponent {
+export class NewHospitalComponent implements OnInit {
+  isLinear = false;
+  hospitalForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.hospitalForm = this.fb.group({
+      hospitalName: [''],
+      contactNo: [''],
+      ownerName: [''],
+      ownerContactNo: [''],
+      ownerEmailId: [''],
+      city: [''],
+      address: [''],
+      userName: [''],
+      password: ['']
+    });
+  }
+  onSubmit() {
 
+  }
+  onReset() {
+
+  }
 }
